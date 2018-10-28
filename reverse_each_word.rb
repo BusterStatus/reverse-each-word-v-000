a2e20e6a58656e53c74a999f695012b0ebfd24a2
matthew.serre <matthew.serre@gmail.com>
2018-10-28 00:41:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,11 @@
 def reverse_each_word(string)
-string = string.split('')
-reversed_string = []
-string.each do |char|
-reversed_string.unshift(char)
-end
-return reversed_string.join('')
+  string = string.split('')
+  
+  reversed_string = []
+  
+  string.each do |char|
+    reversed_string.unshift(char)
+  end
+  
+  return reversed_string.join('')
 end
