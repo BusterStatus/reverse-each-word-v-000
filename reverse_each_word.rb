8e99436322d4b5baa822bd72c088d9f9882cb2b8
matthew.serre <matthew.serre@gmail.com>
2018-10-28 00:33:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,8 @@ def reverse_each_word(string)
   newarray = []
   array = string.split(" ")
   array.each do |element|
+    subarray = element.split
+    subarray.reverse
     newarray.push(element.split)
     newarray.join(" ")
   end
